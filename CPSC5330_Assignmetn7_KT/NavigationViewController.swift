75f2b9134e6778ec89d7d0bf59582206b5243701
Kaique Torres <kaique.serrot@gmail.com>
2024-02-24 14:08:07 -0600

    Part 2 Done // Everything Works // Make better

---
+++ CPSC5330_Assignmetn7_KT/NavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationViewController.swift
+//  CPSC5330_Assignmetn7_KT
+//
+//  Created by Kaique Torres on 2/24/24.
+//
+
+import UIKit
+
+class NavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
